278c502e6912d5f9e55b68cf971f56e659635536
BernardoFOFG <bernardofofg@gmail.com>
2022-11-22 11:39:12 -0300

    fix: overflow

--- src/components/Header/main.js
+++ src/components/Header/main.js
@@ -2,15 +2,12 @@ export default function navMobile() {
   const navBarMobile = document.querySelector(".img-toggle");
   const menuMobile = document.querySelector(".menu-mobile");
   const navBarMobielClose = document.querySelector("#close-mobile");
-  const $html = document.querySelector("html");
 
   navBarMobile.addEventListener("click", () => {
     menuMobile.style.display = "flex";
-    $html.style.overflowY = "hidden";
   });
 
   navBarMobielClose.addEventListener("click", () => {
     menuMobile.style.display = "none";
-    $html.style.overflowY = "auto";
   });
 }
