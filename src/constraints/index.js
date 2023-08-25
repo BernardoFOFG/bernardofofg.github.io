b0ca011fa9dbf3afbfb4e361519de32c52515c38
BernardoFofg-NE <bernardo.guilherme@ne.team>
2023-08-25 02:00:56 -0300

    objects

---
+++ src/constraints/index.js
@@ -0,0 +1,234 @@
+import Sudoku from "../images/sudoku.png";
+import Cronometro from "../images/cronometro.png";
+import FrontEndMentor from "../images/frontend-mentor.png";
+import Cracha from "../images/cracha.png";
+import IAC from "../images/IAC.png";
+import Setup from "../images/nlw-setup.jpg";
+import Search from "../images/buscador.png";
+
+export const Project = [
+  {
+    id: 1,
+    title: "Project Sudoku",
+    description:
+      "This was the first project I developed entirely from scratch, and it had two purposes, the presentation at the college's DemoWeek, and the completion of a grade in the imperative programming chair.",
+    technologies: "Technologies used: HTML, CSS, Javascript. Svelte.",
+    link: "https://projeto-sudoku-ifpe.vercel.app/",
+    linkTitle: "Go to Sudoku",
+    linkRepo: "https://github.com/BernardoFOFG/Projeto-Sudoku-IFPE",
+    linkTitleRepo: "Go to Repository Sudoku",
+    projectPhoto: Sudoku,
+    projectDescription: "Project Sudoku",
+  },
+  {
+    id: 2,
+    title: "IFPE Access Control",
+    description: "Project created for the web development course.",
+    technologies:
+      "Technologies used: HTML, CSS, Javascript, Svelte, TailwindCSS, PHP, MySQL.",
+    linkTitle: "",
+    linkRepo: "https://github.com/BernardoFOFG/iac",
+    linkTitleRepo: "Go to Repository IAC",
+    projectPhoto: IAC,
+    projectDescription: "Project Access Control",
+  },
+  {
+    id: 3,
+    title: "NLW Setup",
+    description:
+      "Project finalized during the event offered by rocketseat, this project helped me kick off 2023, and start the year immersed in programming.",
+    technologies:
+      "Technologies used: HTML5, CSS3, TailwindCSS, Javascript, Typescript, React.JS, Node.js, ReactNative, Expo, Fastify, Axios, Prisma, SQlite, Figma.",
+    linkTitle: "#",
+    linkRepo: "https://github.com/BernardoFOFG/NLW-Setup",
+    linkTitleRepo: "Go to Repository NLW Setup",
+    projectPhoto: Setup,
+    projectDescription: "NLW Setup",
+  },
+  {
+    id: 4,
+    title: "User Search",
+    description:
+      "Simple project where search Github users and your repositories.",
+    technologies:
+      "Technologies used: HTML5, CSS3, Javascript, React.JS, Axios, Http Request",
+    link: "https://buscador-de-usuario.vercel.app/",
+    linkTitle: "Go to Search User",
+    linkRepo: "https://github.com/BernardoFOFG/Buscador-de-Usu-rio",
+    linkTitleRepo: "Go to Repository Search User",
+    projectPhoto: Search,
+    projectDescription: "User Search",
+  },
+  {
+    id: 5,
+    title: "Front End Mentor",
+    description:
+      "SPAs finalized with the aim of learning the fundamentals of front-end development.",
+    technologies: "Technologies used: HTML, CSS, JS, Regex.",
+    link: "https://github.com/BernardoFOFG/Frontend-Mentor",
+    linkTitle: "Go to Challengers",
+    linkRepo:
+      "https://github.com/BernardoFOFG/Frontend-Mentor/tree/main/InteractiveCardDetails",
+    linkTitleRepo: "Go to Recent Project",
+    projectPhoto: FrontEndMentor,
+    projectDescription: "Challengers Frontend Mentor",
+  },
+  {
+    id: 6,
+    title: "StopWatch",
+    description:
+      "Simple project to train some javascript knowledge, and better understand how the setInterval, clearInterval functions worked.",
+    technologies: "Technologies used: HTML, CSS, Javascript.",
+    link: "https://cronometro-bernardofofg.vercel.app/",
+    linkTitle: "Go to StopWatch",
+    linkRepo: "https://github.com/BernardoFOFG/cronometro",
+    linkTitleRepo: "Go to Repository StopWatch",
+    projectPhoto: Cronometro,
+    projectDescription: "Project Stopwatch",
+  },
+  {
+    id: 7,
+    title: "NLW Heat",
+    description:
+      "Event offered by rocketseat for programming students, and the purpose of this application was the API consumption to create an entire page filled with consumption.",
+    technologies: "Technologies used: HTML, CSS, Javascript",
+    link: "Technologies used: HTML, CSS, Javascript",
+    linkTitle: "Go to Crach",
+    linkRepo: "https://github.com/BernardoFOFG/Crach-NLW",
+    linkTitleRepo: "Go to Repository Crach",
+    projectPhoto: Cracha,
+    projectDescription: "NLW Heat",
+  },
+];
+
+export const Front = [
+  {
+    id: 1,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+    alt: "HTML Logo",
+    nome: "HTML",
+  },
+  {
+    id: 2,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+    alt: "CSS Logo",
+    nome: "CSS3",
+  },
+  {
+    id: 3,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+    alt: "Javascript Logo",
+    nome: "Javascript",
+  },
+  {
+    id: 4,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+    alt: "React.JS Logo",
+    nome: "React.JS",
+  },
+  {
+    id: 5,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/svelte/svelte-original.svg",
+    alt: "Svelte Logo",
+    nome: "Svelte",
+  },
+  {
+    id: 6,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+    alt: "Typescript Logo",
+    nome: "Typescript",
+  },
+  {
+    id: 7,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/tailwindcss/tailwindcss-plain.svg",
+    alt: "TailwindCSS Logo",
+    nome: "TailwindCSS",
+  },
+  {
+    id: 8,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/bootstrap/bootstrap-original.svg",
+    alt: "Bootstrap Logo",
+    nome: "Bootstrap",
+  },
+  {
+    id: 9,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/jquery/jquery-original.svg",
+    alt: "jQuery Logo",
+    nome: "jQuery",
+  },
+];
+
+export const Back = [
+  {
+    id: 1,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/php/php-plain.svg",
+    alt: "PHP Logo",
+    nome: "PHP",
+  },
+  {
+    id: 2,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+    alt: "NodeJS Logo",
+    nome: "Node.JS",
+  },
+  {
+    id: 3,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mysql/mysql-original.svg",
+    alt: "MYSQL Logo",
+    nome: "MYSQL",
+  },
+  {
+    id: 4,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/laravel/laravel-plain.svg",
+    alt: "Laravel Logo",
+    nome: "Laravel",
+  },
+  {
+    id: 5,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/symfony/symfony-original.svg",
+    alt: "Symfony Logo",
+    nome: "Symfony",
+  },
+];
+
+export const Code = [
+  {
+    id: 1,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vscode/vscode-original.svg",
+    alt: "VsCode Logo",
+    nome: "Visual Studio Code",
+  },
+  {
+    id: 2,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/intellij/intellij-original.svg",
+    alt: "Intellij Logo",
+    nome: "Intellij",
+  },
+  {
+    id: 3,
+    img: "https://cdn.worldvectorlogo.com/logos/sublime-text.svg",
+    alt: "Sublime Logo",
+    nome: "Sublime Text",
+  },
+];
+
+export const Extras = [
+  {
+    id: 1,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg",
+    alt: "Git Logo",
+    nome: "Git",
+  },
+  {
+    id: 2,
+    img: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/materialui/materialui-original.svg",
+    alt: "Material UI Logo",
+    nome: "Material UI",
+  },
+  {
+    id: 3,
+    img: "https://img.icons8.com/fluency-systems-filled/48/4FD1C5/chakra-ui.png",
+    alt: "ChakraUI Logo",
+    nome: "Chakra.UI",
+  },
+];
